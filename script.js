16e255a256446be105d5bec44e59990c3a8e40e1
Jayana10 <jayana10302@gmail.com>
2025-02-27 22:29:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 //your JS code here. If required.
 const student = {
-	name:john,
+	name:"john",
 	age: 30,
     city:"new york"
 };
+
 function getKeys(obj) {
 	 return Object.keys(obj);
 }
+const keys = getKeys(myObj);
 console.log(getKeys(student));
