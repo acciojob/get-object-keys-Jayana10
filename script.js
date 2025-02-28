c836eaa28d8429bf206a4935454b33a10c55d60b
Jayana10 <jayana10302@gmail.com>
2025-02-28 12:19:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 //your JS code here. If required.
-const student = {
-	name:"john",
-	age: 30,
-    city:"new york"
-};
 
 Object.prototype.getKeys = function(){
 	return Object.getKeys(this);
 }
-console.log(student.getKeys());
+window.student = { name: "John Doe" };
