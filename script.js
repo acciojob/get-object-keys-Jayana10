3d2e64008878d2da2a669e1b2b12f0cb40a14823
Jayana10 <jayana10302@gmail.com>
2025-02-28 12:23:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 //your JS code here. If required.
 
-Object.prototype.getKeys = function(){
-	return Object.Keys(this);
-}
+// Object.prototype.getKeys = function(){
+// 	return Object.Keys(this);
+// }
+// window.student = { name: "John Doe" };
+Object.prototype.getKeys = function () {
+    return Object.keys(this);
+};
+
+// Do not change the code below this line
 window.student = { name: "John Doe" };
